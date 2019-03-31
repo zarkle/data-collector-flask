f9f26b03a1cd4a4808b6abde1228f86d3ab5dcda
Beverly Pham <beverly.pham@gmail.com>
2019-03-30 17:30:46 -0700

    add docstrings

--- app.py
+++ app.py
@@ -3,13 +3,16 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """Home route."""
     return render_template('index.html')
 
 
 @app.route('/success', methods=['POST'])
 def success():
+    """Success route."""
     if request.method=='POST':
         email = request.form['email_name']
         height = request.form['height']
