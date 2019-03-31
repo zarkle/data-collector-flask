5d2f586c81fa94de554d8b068e11c3dc9bdd0ce6
Beverly Pham <beverly.pham@gmail.com>
2019-03-30 17:02:16 -0700

    after form submit, routes to success page

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/success', methods=['POST'])
+def success():
+    return render_template('success.html')
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run()
