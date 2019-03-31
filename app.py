48d958a783305409bb8a875d9c69bcdb4d18de8c
Beverly Pham <beverly.pham@gmail.com>
2019-03-30 17:14:21 -0700

    basic app working, form data is getting sent to  backend

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/success', methods=['POST'])
 def success():
-    return render_template('success.html')
+    if request.method=='POST':
+        email = request.form['email_name']
+        height = request.form['height']
+        return render_template('success.html')
 
 
 if __name__ == '__main__':
