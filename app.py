23b71cba11f8f94af6efd8b1928b920a82671eb7
Beverly Pham <beverly.pham@gmail.com>
2019-03-30 18:15:29 -0700

    create database model

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, request
+from flask.ext.sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://user:@localhost/data_collector'
 
+db = SQLAlchemy(app)
+
+
+class Data(db.Model):
+    """Data Model."""
+    __tablename__ = 'height'
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True)
+    height = db.Column(db.Integer)
 
 @app.route('/')
 def index():
